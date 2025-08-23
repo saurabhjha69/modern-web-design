173690b518271e1836677f7e7720ee32d6ab76d2
saurabhjha69 <saurabhjhaforpc69@gmail.com>
2025-08-23 20:19:02 +0530

    Completed whole landing page

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ let observer = new IntersectionObserver((entries) => {
     })
 },{threshold: 0,rootMargin: "0px 0px -100% 0px"})
 observer.observe(underLayingImage)
+
+const testimonialCarouselContainer = document.querySelector('.testimonials')
+function scrollTestimonialContainer(index) {
+    let scrollingWidth = testimonialCarouselContainer.clientWidth *index
+    testimonialCarouselContainer.scrollTo({
+        left: scrollingWidth,
+        behavior: "smooth"
+    })
+}
+
+let testimonialCarouselNavDots = document.querySelectorAll(".navigation-dots ul li button")
+testimonialCarouselNavDots.forEach((dot,index) => {
+    dot.addEventListener('click',(e)=> {
+        console.log("clicked!")
+        if(!e.target.classList.contains('active')){
+            console.log("Hey!")
+            let activeDot = document.querySelector(".navigation-dots ul li button.active")
+            activeDot.classList.remove('active')
+            scrollTestimonialContainer(index)
+            e.target.classList.add('active')
+
+        }
+    })
+    // dot.classList.toggle('active',)
+})
